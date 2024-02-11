e7f927375d7938ed20fb6fa3ecd7165f9af71d18
VL <veranikalis11@gmail.com>
2024-02-11 15:29:44 +0100

    HW

---
+++ lesson4/HW/Program.cs
@@ -0,0 +1,27 @@
+﻿ string GetNumbers(string s)
+{
+    string numbers = "";
+    foreach (char e in s)
+    {
+        if (char.IsNumber(e) == true)
+        {
+            numbers = numbers + ", " + e;
+        }
+    }
+    return numbers;
+}
+
+string str = Console.ReadLine();
+string semires = GetNumbers(str);
+// int numbers = Convert.ToInt32(semires);
+
+
+//  int[]array = new int [];
+// while (numbers > 0)
+//  {
+//     int j=size-1;
+//    int digit = numbers %10;
+//     numbers /= 10;
+//    array[j]=digit;
+//  }
+Console.WriteLine($"Массив: [{semires}]");
